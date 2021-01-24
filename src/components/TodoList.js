9494eb858e34d4ae9f9c886b62b8541cce04bcf0
Fanksv2 <fanksv2@gmail.com>
2021-01-24 20:48:49 -0300

    Added - Request for the backend for get todo list data

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,29 @@
 import React, {Component} from 'react';
 import Task from "./Task";
+import api from "../services/api";
 
 class TodoList extends Component {
 
-
-  // {
-  //   "_id": "60062e215ea4951e28edef7c",
-  //   "content": {
-  //     "text": "usahusahsau",
-  //     "status": "todo"
-  //   },
-  //   "createdAt": "2021-01-19T00:56:01.859Z",
-  //   "updatedAt": "2021-01-19T00:56:01.859Z",
-  //   "__v": 0
-  // }
-
-
+  state = {
+    tasks:[]
+  }
+  
+  async componentDidMount(){
+    const res = await api.get("todo");
+    this.setState({tasks: res.data});
+  }
   
   render(){
     return (
       <section className="todo-area">
         <h2>TO DO</h2>
-        <Task data={undefined}/>
+        {
+          this.state.tasks.map(task =>(
+              <Task data={task} key={task._id}/>
+            )
+          )
+          
+        }
       </section>
     )
   }
