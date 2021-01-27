f4268f2e2d297245a0682e6cc0a2730fa9aaf01a
YukioVN <noma@alunos.utfpr.edu.br>
2021-01-26 22:42:30 -0300

    Added request function in DoneList.js

--- src/components/DoneList.js
+++ src/components/DoneList.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Task from './Task';
+import api from "../services/api";
 
-function DoneList() {
-  return (
-    <section className="done-area">
-      <h2>DONE</h2>
-      <Task/>
-    </section>
-  );
+class DoneList extends Component {
+
+  state = {
+    tasks:[]
+  }
+
+  async componentDidMount(){
+    const res = await api.get("done");
+    this.setState({tasks: res.data});
+  }
+
+  render() {
+    return (
+      <section className="done-area">
+        <h2>DONE</h2>
+        {
+          this.state.tasks.map(task =>(
+              <Task data={task} key={task._id}/>
+            )
+          )
+        }
+      </section>
+    );
+  }
+  
 }
 
 export default DoneList;
