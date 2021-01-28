5e0931759283fcee7844182a75e174da52160cde
Gabriel Augusto Arruda <gabriel.arruda@oktagon.com.br>
2021-01-28 20:20:43 -0300

    Added - "Enter" now add tasks, Text is cleared on "enter", input field now fill all the space

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,35 +1,55 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import { IconContext } from "react-icons";
 import { AiOutlinePlus } from "react-icons/ai";
 import api from "../services/api";
 
-function TodoInput() {
-  const [Task,setTask] = useState("");
+class TodoInput extends Component {
+  //const [Task,setTask] = useState("");
+  state = {
+    task : "",
+    input : undefined
+  }
   
-  async function Input(){
+  async Input(){
+    if(this.state.task === "")
+      return;
+
     const conteudo = {
       data: {
-        text: Task,
+        text: this.state.task,
         status: "todo"
       }
     }
+    console.log(conteudo);
     await api.post("create", conteudo);
   }
-  return (
-    <div className="input-container">
-      <input
-        className="addTask" 
-        type="text"
-        placeholder="Adicione uma tarefa"
-        onChange = {(evento) => setTask(evento.target.value)}
-      />
-      <IconContext.Provider value={{color:"#00B0FF", size:"20px"}}>
-      <div className='icon-container'>
-        <button className='plus-button' onClick={Input}><AiOutlinePlus/></button>
-      </div>
-      </IconContext.Provider>
-    </div>
-  );
+
+  render(){
+    return (
+      <form className="input-container" onSubmit={(event) => this.handleSubmit(event)}>
+          <input
+            className="addTask" 
+            type="text"
+            placeholder="Adicione uma tarefa"
+            onChange = {(evento) => this.setState({task : evento.target.value})}
+            value = {this.state.task}
+          />
+          <IconContext.Provider value={{color:"#00B0FF", size:"20px"}}>
+          <div className='icon-container'>
+            <button className='plus-button'  onClick={() => this.Input()}><AiOutlinePlus/></button>
+          </div>
+        </IconContext.Provider>
+      </form>
+    );
+  }
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState({
+      task : ""
+    });
+  }
 }
+
+
 
 export default TodoInput;
